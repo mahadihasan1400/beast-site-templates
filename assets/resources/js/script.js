33d7c719f35e65fe12e3c997f544bacabbaeccb0
mahadihasan1400 <mahadihasan1400@gmail.com>
2018-07-17 21:45:57 +0600

    14th commit

--- assets/resources/js/script.js
+++ assets/resources/js/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
+    /*G MAPS */
+    var lat = 38.722252;
+    var lng = -9.139337;
 
+    var map = new GMaps({
+        div: '.map',
+        lat: lat,
+        lng: lng,
+        zoom: 12
+    });
+
+    map.addMarker({
+        lat: lat,
+        lng: lng,
+        title: 'Lima',
+        click: function (e) {
+            alert('You clicked in this marker');
+        }
+    });
+
+
+    // $('h2').click(function () {
+    //     $(this).css('background-color','#ff0000');
+    // });
 
 
 });
